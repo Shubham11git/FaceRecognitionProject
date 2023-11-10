c62e369c27f578c55aaa9c83b85acc21f454863b
Shubham11git <110545090+Shubham11git@users.noreply.github.com>
2023-11-10 13:45:50 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -28,4 +28,4 @@ contents = get_directory_contents(username, repo_name, directory_path)
 
 if contents:
     for item in contents:
-        st.write(f"Name: {item['name']}, Type: {item['type']}")
+        st.write(f"Name: {item['name'].split('.')[0]}, Type: {item['type']}")
