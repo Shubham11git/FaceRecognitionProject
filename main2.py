da95f4b0559eb9f17b4268c9961b334f73aa1c5f
Shubham11git <110545090+Shubham11git@users.noreply.github.com>
2023-11-10 13:20:51 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -7,8 +7,8 @@ import tempfile
 
 import requests
 
-def get_directory_contents(base_url): #username, repo_name, path, branch='master'):
-    # base_url = f'https://api.github.com/repos/{username}/{repo_name}/contents/{path}?ref={branch}'
+def get_directory_contents(username, repo_name, path, branch='master'):
+    base_url = f'https://api.github.com/repos/{username}/{repo_name}/contents/{path}?ref={branch}'
     response = requests.get(base_url)
 
     if response.status_code == 200:
@@ -18,14 +18,14 @@ def get_directory_contents(base_url): #username, repo_name, path, branch='master
         st.write(f"Failed to retrieve directory contents. Status code: {response.status_code}")
         return None
 
-# Replace these with your GitHub username, repository name, and directory path
-# username = 'Shubham11git'
-# repo_name = 'FaceRecognitionProject'
-# directory_path = 'images'
+Replace these with your GitHub username, repository name, and directory path
+username = 'Shubham11git'
+repo_name = 'FaceRecognitionProject'
+directory_path = 'images'
 
-url = 'https://github.com/Shubham11git/FaceRecognitionProject/tree/main/images'
-contents = get_directory_contents(url)
+# url = 'https://github.com/Shubham11git/FaceRecognitionProject/tree/main/images'
+contents = get_directory_contents(username, repo_name, directory_path)
 
-# if contents:
-#     for item in contents:
-#         st.write(item.split('.')[0])
+if contents:
+    for item in contents:
+        st.write(item.split('.')[0])
