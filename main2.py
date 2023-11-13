500bec3d1fa10537457c48dc5e77a2bc34f39246
Shubham11git <khobragadeshubham55@gmail.com>
2023-11-13 12:27:15 +0530

    Modify main and main2

--- main2.py
+++ main2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-def get_directory_contents(username, repo_name, path, branch='main'):
+def get_image_names(username, repo_name, path, branch='main'):
     base_url = f'https://api.github.com/repos/{username}/{repo_name}/contents/{path}?ref={branch}'
     response = requests.get(base_url)
 
@@ -11,15 +11,32 @@ def get_directory_contents(username, repo_name, path, branch='main'):
     else:
         st.write(f"Failed to retrieve directory contents. Status code: {response.status_code}")
         return None
+    
+
+def get_image_urls(username, repo_name, path, branch='main'):
+    base_url = f'https://api.github.com/repos/{username}/{repo_name}/contents/{path}?ref={branch}'
+    response = requests.get(base_url)
+
+    if response.status_code == 200:
+        contents = response.json()
+        image_urls = []
+        for item in contents:
+            if item['type'] == 'file' and item['name'].lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
+                raw_url = item['download_url'].replace('/downloads/', '/raw/')  # Convert download_url to raw_url
+                image_urls.append(raw_url)
+        return image_urls
+    else:
+        print(f"Failed to retrieve directory contents. Status code: {response.status_code}")
+        return None
 
 # Replace these with your GitHub username, repository name, and directory path
-username = 'Shubham11git'
-repo_name = 'FaceRecognitionProject'
-directory_path = 'images'
+# username = 'Shubham11git'
+# repo_name = 'FaceRecognitionProject'
+# directory_path = 'images'
 
 
-contents = get_directory_contents(username, repo_name, directory_path)
+# contents = get_image_names(username, repo_name, directory_path)
 
-if contents:
-    for item in contents:
-        st.write(f"Name: {item['name'].split('.')[0]}, Type: {item['type']}")
+# if contents:
+#     for item in contents:
+#         st.write(f"Name: {item['name'].split('.')[0]}, Type: {item['type']}")
