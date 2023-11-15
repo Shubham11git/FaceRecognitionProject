d04a4a2c02ce58a43dab2134a0fbfa29cba87187
Shubham11git <khobragadeshubham55@gmail.com>
2023-11-15 15:29:19 +0530

    Updated test.py

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
 
-class VideoProcessor(VideoProcessorBase):
-    def __init__(self):
-        # Define any initialization code here
-        pass
+# class VideoProcessor(VideoProcessorBase):
+#     def __init__(self):
+#         # Define any initialization code here
+#         pass
 
-    def process(self, frame):
-        # Process the frame (e.g., apply filters, perform analysis)
-        # In this example, we simply return the frame without any processing
-        return frame
+#     def process(self, frame):
+#         # Process the frame (e.g., apply filters, perform analysis)
+#         # In this example, we simply return the frame without any processing
+#         return frame
 
 def main():
     st.title("Webcam with Streamlit WebRTC")
@@ -17,13 +17,13 @@ def main():
     # Create a WebRTC component with the VideoTransformer class
     webrtc_ctx = webrtc_streamer(
         key="example",
-        video_processor_factory=VideoProcessor,
-        async_processing=True,
+        # video_processor_factory=VideoProcessor,
+        # async_processing=True,
     )
 
     # Display the webcam feed
-    if webrtc_ctx.video_processor:
-        st.video(webrtc_ctx.video_processor)
+    # if webrtc_ctx.video_processor:
+    #     st.video(webrtc_ctx.video_processor)
 
 if __name__ == "__main__":
     main()
